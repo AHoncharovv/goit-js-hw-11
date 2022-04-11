739d3db6cd13573317161efc922fb05cb2d68bcb
Anton Honcharov <antonhoncharovv@gmail.com>
2022-04-11 16:46:00 +0300

    add photo cards

--- src/index.js
+++ src/index.js
@@ -6,26 +6,40 @@ const axios = require('axios');
 const BASE_URL = 'https://pixabay.com/api/';
 const LIBRARY_KEY = '26656666-c9df0a89ed3cb80a5684720fa';
 const inputField = document.querySelector('.search-form');
+const photoCardField = document.querySelector('.gallery');
+const loadMoreBtn = document.querySelector('.load-more');
 
 let inputText = '';
 let searchPage = 1;
+
+inputField.style.marginBottom = "25px";
+loadMoreBtn.style.marginTop = "25px";
+loadMoreBtn.style.marginLeft = "250px";
+loadMoreBtn.style.marginBottom = "25px";
+loadMoreBtn.style.opacity = "0";
 
 inputField.addEventListener("submit", handleSubmit);
 
 function handleSubmit(event) {
   event.preventDefault();
+  photoCardField.innerHTML = '';
   inputText = event.currentTarget.elements.searchQuery.value;
-  console.log(inputText);
-
+ 
   getUser().then(response => {
       if (response.data.total === 0) {
         Notify.failure('Sorry, there are no images matching your search query. Please try again.');    
-    }
-    console.log(response.data.hits[23].largeImageURL);
+        loadMoreBtn.style.opacity = "0";
+        return;
+    };
+    return response.data.hits;
+  }).then(result => {
+    const photoListMarkup = result.map(makeCardMarkup).join('');
+      photoCardField.insertAdjacentHTML('beforeend', photoListMarkup);
   });
   
   searchPage = 1;
   event.target.reset(); 
+  loadMoreBtn.style.opacity = "1";
 }
 
 async function getUser() {
@@ -38,7 +52,7 @@ async function getUser() {
             orientation: 'horizontal',
             safesearch: 'true',
             page: searchPage,
-            per_page: 40,
+            per_page: 3,
 
     }
     });
@@ -49,6 +63,43 @@ async function getUser() {
   }
 };
 
+const makeCardMarkup = (element) => {
+    return      `<div class="photo-card">
+                <img src="${element.webformatURL}" alt="${element.tags}" loading="lazy" />
+                <div class="info">
+                  <p class="info-item">
+                    <b>Likes: ${element.likes}</b>
+                  </p>
+                  <p class="info-item">
+                    <b>Views: ${element.views}</b>
+                  </p>
+                  <p class="info-item">
+                    <b>Comments: ${element.comments}</b>
+                  </p>
+                  <p class="info-item">
+                    <b>Downloads: ${element.downloads}</b>
+                  </p>
+                </div>
+              </div>`;
+};
+
+loadMoreBtn.addEventListener('click', handleLoadMoreClick);
+
+function handleLoadMoreClick(element) {
+  searchPage += 1;
+
+  getUser().then(response => {
+      if (searchPage > response.data.totalHits) {
+        Notify.failure("We're sorry, but you've reached the end of search results.");  
+        loadMoreBtn.style.opacity = "0";
+        return;
+    };
+    return response.data.hits;
+  }).then(result => {
+    const photoListMarkup = result.map(makeCardMarkup).join('');
+      photoCardField.insertAdjacentHTML('beforeend', photoListMarkup);
+  });
+}
 
 
 
